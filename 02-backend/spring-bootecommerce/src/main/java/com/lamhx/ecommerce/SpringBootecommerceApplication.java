de8377ad4c7f243064b59c7783584abdd665a2af
Ho-Xuan-Lam <61542100+Ho-Xuan-Lam@users.noreply.github.com>
2021-05-28 21:33:46 +0700

    push
    config swagger 2

--- 02-backend/spring-bootecommerce/src/main/java/com/lamhx/ecommerce/SpringBootecommerceApplication.java
+++ 02-backend/spring-bootecommerce/src/main/java/com/lamhx/ecommerce/SpringBootecommerceApplication.java
@@ -3,7 +3,13 @@ package com.lamhx.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+
+
+
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class SpringBootecommerceApplication {
 
 	public static void main(String[] args) {
